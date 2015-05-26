fc08a9bfe07b6fc3513bb70b003d38b66667690b
Benedikt Schmidt <benedikt.schmidt01@sap.com>
2015-05-26 08:07:16 +0200

    [FEATURE] valid check for sequences

--- BioInfo/src/main/java/com/bio/graph/GraphFactory.java
+++ BioInfo/src/main/java/com/bio/graph/GraphFactory.java
@@ -2,17 +2,30 @@ package com.bio.graph;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.bio.sequence.Sequence;
 
 public class GraphFactory {
+	
+	private static final Logger logger = LogManager.getLogger(GraphFactory.class);
 
 	public static DNAGraph createFromSequences(List<String> rawSequences) {
 		DNAGraph graph = new DNAGraph();
 		long i = 1l;
+		long errorCount=0;
 		for (String nextLine : rawSequences) {
 			Sequence sequence = new Sequence(nextLine);
-			graph.insertNewSequence(sequence, i);
-			i++;
+			if (sequence.isValid()) {
+				graph.insertNewSequence(sequence, i);
+				i++;
+			} else {
+				errorCount++;
+			}
+		}
+		if(errorCount>0){
+			logger.warn(errorCount+" sequences were not valid!");
 		}
 		return graph;
 	}
